e1fb0786dbaa63122165103635da6dcd511fad7b
BTV <horsfalm@gmail.com>
2022-01-15 09:31:18 +1100

    begin establishing subclasses

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,10 @@
-//const Manager = require('../lib/Manager.js')
+const Manager = require('../lib/Manager')
+
+test('creates a manager object', () => {
+    const manager = new Manager('Dave');
+
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+})
