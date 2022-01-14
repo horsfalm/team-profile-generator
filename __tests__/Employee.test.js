48ede0894d11cd5427ed7f501303bc8316797529
BTV <horsfalm@gmail.com>
2022-01-15 09:06:50 +1100

    create initial employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee.js')
+
+test('creates an employee object', () => {
+    const employee = new Employee();
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+})
