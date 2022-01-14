6f631cbb465cf4bb114afe68ac1da3d66a9dd071
BTV <horsfalm@gmail.com>
2022-01-15 10:17:44 +1100

    test and then resolve employee methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,33 @@
 const Employee = require('../lib/Employee')
 
-test('creates an employee object', () => {
-    const employee = new Employee();
+test('creates a employee object', () => {
+    const employee = new Employee('Dave');
 
-    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-})
+});
+
+test('gets employee name', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getName()).toBe('Dave');
+});
+
+test('gets employee id', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test('gets employee role', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,1 @@
-const Manager = require('../lib/Manager')
-
-test('creates a manager object', () => {
-    const manager = new Manager('Dave');
-
-    expect(manager.name).toBe('Dave');
-    expect(manager.id).toEqual(expect.any(Number));
-    expect(manager.email).toEqual(expect.any(String));
-    expect(manager.officeNumber).toEqual(expect.any(Number));
-})
+//const Manager = require('../lib/Manager')
