afb701366d44dff8ce91579c0d629a5e6ed2c725
BTV <horsfalm@gmail.com>
2022-01-17 17:19:22 +1100

    improve user input code

--- index.js
+++ index.js
@@ -5,15 +5,213 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-team = [];
+const team = [];
 
-console.log('Please enter Manager details:');
+function getManager() {
+    console.log("Build your Team!");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managersName",
+            message: "Please enter the Manager's name.",
+            validate: answer => {
+                if (answer === "") {
+                    console.log("Please enter a name for your Manager.");
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: "number",
+            name: "managersId",
+            message: "Please enter the Manager's id.",
+            validate: answer => {
+                if (answer < 0) {
+                    console.log("Please enter a number greater than zero.");
+                    return false;    
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "managersEmail",
+            message: "Please enter the Manager's email.",
+            validate: answer => {
+                let passAnswer = answer.match(/\S+@\S+\.\S+/);
+                if (passAnswer) {
+                    return true;
+                }
+                console.log("Please enter a valid email address.");
+                return false;
+            }
+        },
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "Please enter the Manager's office number.",
+            validate: answer => {
+                if (answer < 0) {
+                    console.log("Please enter a number greater than zero.");
+                    return false;
+                }
+                return true;
+            }
+        }
+    ]).then(answers => {
+        const manager = new Manager(answers.managersName, answers.managersId, answers.managersEmail, answers.officeNumber);
+        team.push(manager);
+        addTeam();
+    });
+};
 
-const promptManager = () => {
-    new Manager().getName();
+function addTeam() {
+    inquirer
+    .prompt([{
+        type: 'list',
+        name: 'memberRole',
+        message: 'Would you like to add another member to your team?',
+        choices: ['Engineer', 'Intern', 'Exit']
+    }]).then(chosen => {
+        switch (chosen.memberRole) {
+            case 'Engineer':
+                addEngineer();
+                break;
+                case 'Intern':
+                    addIntern();
+                    break;
+                    case 'Exit':
+                        buildTeam();
+                        break;
+        }
+    });
+};
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "Please enter the name of your Engineer.",
+            validate: answer => {
+                if (answer === "") {
+                    console.log("Please enter a name for you Engineer.");
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'number',
+            name: 'engineerId',
+            message: 'Please enter the Engineer ID.',
+            validate: answer => {
+                if (answer < 0) {
+                    console.log('Please enter a number greater than zero.');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "Please enter the Engineer's email",
+            validate: answer => {
+                let passAnswer = answer.match(/\S+@\S+\.\S+/);
+                if (passAnswer) {
+                    return true;
+                }
+                console.log("Please enter a valid email address.");
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'Enter the Github username of your Engineer.',
+            validate: answer => {
+                if (answer === "") {
+                    console.log("Please enter a Github username for your Engineer.");
+                    return false;
+                }
+                return true;
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        team.push(engineer);
+        addTeam();
+    })
+};
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'Please enter the name of your intern.',
+            validate: answer => {
+                if (answer === "") {
+                    console.log('Please enter a name for your Intern.');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'number',
+            name: 'internId',
+            message: 'Please enter the ID for your Intern.',
+            validate: answer => {
+                if (answer < 0) {
+                    console.log('Please enter a number greater than zero.');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'Please enter the Intern email.',
+            validate: answer => {
+                let passAnswer = answer.match(/\S+@\S+\.\S+/);
+                if (passAnswer) {
+                    return true;   
+                }
+                console.log('Please enter a valid email address.');
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'Please enter the name of the school for your Intern.',
+            validate: answer => {
+                if (answer === "") {
+                    console.log('Please enter a school for your Intern.');
+                    return false;
+                }
+                return true;
+            }
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        team.push(intern);
+        addTeam();
+    })
+};
+
+function buildTeam() {
+console.log(team);
+    //     fs.writeFileSync('./dist/index.html', renderPage(team), 'UTF-8');
 }
 
-promptManager();
+getManager();
+
+
 
 //const promptMgr = () => {
 //     return inquirer.prompt([
